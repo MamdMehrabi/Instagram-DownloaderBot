43eb1e44fd014e8c484d13d737ec1c5c34495b31
MamdMehrabi <mohammadmehrabi175@gmail.com>
2024-12-19 21:30:03 +0330

    add errors handle

--- Insta-dl/main.py
+++ Insta-dl/main.py
@@ -8,11 +8,20 @@ async def main(c, m):
     
     if 'instagram.com' in m.text:
         url = m.text
+        proccessing = await m.reply("در حال پردازش...", reply_to_message_id=m.id)
         json_api = requests.get(f"https://nativeig.vercel.app/api/video?postUrl={url}").json()
-        video_url = json_api['data']['videoUrl']
-        width = json_api['data']['width']
-        height = json_api['data']['height']
-        await app.send_video(chat_id = m.chat.id, video = video_url, caption = f"📹 {width} x {height}\n🪄 @TheCommit")
+        if json_api['status'] == 'success':
+            video_url = json_api['data']['videoUrl']
+            width = json_api['data']['width']
+            height = json_api['data']['height']
+            await app.send_video(chat_id = m.chat.id, video = video_url, caption = f"📹 {width} x {height}\n🪄 @TheCommit", reply_to_message_id = m.id)
+            await app.delete_messages(chat_id=m.chat.id, message_ids=proccessing.id)
+        elif json_api['status'] == 'error':
+            await m.reply(f"❌ به مشکل خوردیم\n❗{json_api['message']}", reply_to_message_id = m.id)
+            await app.delete_messages(chat_id=m.chat.id, message_ids=proccessing.id)
+        else:
+            await m.reply("🤔ارور ناشناخته", reply_to_message_id = m.id)
+            await app.delete_messages(chat_id=m.chat.id, message_ids=proccessing.id)
 
 if __name__ == '__main__':
     app.run()
